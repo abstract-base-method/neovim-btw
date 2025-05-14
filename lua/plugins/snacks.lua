7c5835b854882631fed40ac0ded156dbe755b046
James Edward Bell Jr. <13918870+abstract-base-method@users.noreply.github.com>
2025-05-14 09:45:57 -0500

    feat(snacks):refactor to pure snacks
    or close to it
    
    Signed-off-by: James Edward Bell Jr. <13918870+abstract-base-method@users.noreply.github.com>

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,32 +1,25 @@
 return {
-	{
-		"folke/snacks.nvim",
-		priority = 1000,
-		lazy = false,
-		---@type snacks.Config
-		opts = {
-			-- your configuration comes here
-			-- or leave it empty to use the default settings
-			-- refer to the configuration section below
-			bigfile = { enabled = true },
-			dashboard = { enabled = true },
-			indent = { enabled = true },
-			input = { enabled = true },
-			picker = {
-				enabled = true,
-				layout = {
-					layout = {
-						position = "right",
-					},
-				},
-			},
-			notifier = { enabled = true },
-			quickfile = { enabled = true },
-			scroll = { enabled = true },
-			statuscolumn = { enabled = true },
-			words = { enabled = true },
-		},
-		keys = {
+  "folke/snacks.nvim",
+  priority = 1000,
+  lazy = false,
+  ---@type snacks.Config
+  opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+    bigfile = { enabled = true },
+    dashboard = { enabled = true },
+    explorer = { enabled = true },
+    indent = { enabled = true },
+    input = { enabled = true },
+    picker = { enabled = true },
+    notifier = { enabled = true },
+    quickfile = { enabled = true },
+    scope = { enabled = true },
+    scroll = { enabled = true },
+    statuscolumn = { enabled = true },
+    words = { enabled = true },
+    keys = {
 			{ "<leader>sf", function() Snacks.picker.files() end,               desc = "Find files" },
 			{ "<leader>sb", function() Snacks.picker.buffers() end,             desc = "Find buffer" },
 			{ "<leader>sg", function() Snacks.picker.grep() end,                desc = "Search Grep" },
@@ -70,5 +63,5 @@ return {
 				end,
 			}
 		},
-	}
+  },
 }
