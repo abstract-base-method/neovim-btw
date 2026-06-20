dff9c4111c023b9c39dfc59108ef5a63ea9c43fc
James E. Bell Jr <13918870+abstract-base-method@users.noreply.github.com>
2026-06-20 09:02:35 -0500

    feat(theme):reset to eldrich
    Signed-off-by: James E. Bell Jr <13918870+abstract-base-method@users.noreply.github.com>

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,12 +1,20 @@
 return {
 	{
-		"folke/tokyonight.nvim",
+		"eldritch-theme/eldritch.nvim",
+		commit = "c9131a5a11f00a2f428f563a4eb2c4aeb680d963",
+		priority = 1000,
 		opts = {
 			transparent = true,
 			styles = {
 				sidebars = "transparent",
 				floats = "transparent",
 			},
+		},
+	},
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "eldritch",
 		},
 	},
 }
